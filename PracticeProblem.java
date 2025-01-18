2ab39bc1ee61b8a6a453ab2ec134f51a29a442dd
nikhilkumar2002 <nikunikhil0409@gmail.com>
2025-01-18 15:00:07 +0530

    even/odd

---
+++ PracticeProblem.java
@@ -0,0 +1,10 @@
+public  class Helloworld{
+	public static void main(String x[]){
+		for(int i=1;i<=10;i++){
+			if(i%2==0)
+				System.out.println(i+" is even number");
+			else
+				System.out.println(i+" is odd nubmer");
+		}
+	}
+}
